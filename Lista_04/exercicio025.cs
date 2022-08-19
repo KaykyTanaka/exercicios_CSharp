b581f9b796b2bccc914a3098d60e94df2d9367b9
unknown <tanaka.kayky@gmail.com>
2022-08-19 20:02:17 -0300

    exercícios C#

--- Lista_04/exercicio025.cs
+++ Lista_04/exercicio025.cs
@@ -1,8 +1,11 @@
 ﻿/* - Criar um algoritmo que calcule e imprima a área e a hipotenusa de um triângulo
 retângulo.
-Observação: Os valores devem ser positivos.
+Observação: Os valores devem ser positivos.*/
 
-b.h /2 
- */
+Console.WriteLine("Insira a base do triangulo: ");
+int baseT = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Insira a altura: ");
+int altura = int.Parse(Console.ReadLine());
 
+Console.WriteLine($"O valor da hipotenusa é: {(baseT*altura)/2}");
